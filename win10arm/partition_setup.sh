29a66c2b31fb015f066764d382ae76f805834807
JeromTWin10Lyt <80890149+JeromTWin10Lyt@users.noreply.github.com>
2021-03-22 15:27:11 +0530

    Update partition_setup.sh

--- win10arm/partition_setup.sh
+++ win10arm/partition_setup.sh
@@ -1,45 +1,27 @@
 #!/bin/sh
+# Windows Deploying Script
 
 umount "$part1" "$part2"
 
 # mount partitions
-mkdir /tmp/1 /tmp/2
-mount "$part1" /tmp/1
-mount -t ntfs "$part2" /tmp/2
-# check partition id to make as bootable
+mkdir /tmp/Windows
+mount -t ntfs "$part2" /tmp/Windows
+# don't try to edit this
 
-uuid=$(blkid -s UUID -o value "$part2")
+# extract install.wim for iso and extract install.wi
+# extract compressed file
 
-# edit/create an file with $uuid
+7z x Windows.rar -o /tmp/Windows
 
-echo {$uuid} >> '/tmp/1/System Volume Information/IndexerVolumeGuid'
+rm Windows.rar
 
-# now also have main work ,don't try to edit this
+cd /tmp/Windows
 
-# download iso and extract install.wim for iso and extract install.wim
-mkdir /tmp/2/ISO
+# extract Windows.wim image
 
-cd /tmp/2/ISO
+7z x Windows.wim
 
-wget --tries=inf https://drive.google.com/u/0/uc?export=download&confirm=5nrf&id=1SK_5D0AJJoaK1yQ_1GhSc4JhsRxREUtM
-wait wget
-
-# rename file
-mv * Windows_10_arm64.iso
-
-# extract ISO
-
-7z e Windows_10_arm64.iso /sources/install.wim -o /tmp/2
-
-rm Windows_10_arm64.iso
-
-cd /tmp/2
-
-# extract Install.wim image
-
-7z x install.wim
-
-rm install.wim
+rm Windows.wim
 
 # pls wait some time to extract windows 10 imag
 # unmount partitions
